9fd4ee9b5a1af5f3d192ae1add3d4ebdc4323434
Noah Kim <noahbkim@gmail.com>
2020-06-22 15:37:26 -0400

    Implement code action providers, resolvers

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.VSCode/src/CodeActions/RazorCodeActionRunner.ts
@@ -0,0 +1,35 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as vscode from 'vscode';
+import { RazorLanguageServerClient } from '../RazorLanguageServerClient';
+import { RazorLogger } from '../RazorLogger';
+import { CodeActionResolutionResponse } from '../RPC/CodeActionResolutionResponse';
+import { convertWorkspaceEditFromSerializable } from '../RPC/SerializableWorkspaceEdit';
+
+export class RazorCodeActionRunner {
+
+    constructor(
+        private readonly serverClient: RazorLanguageServerClient,
+        private readonly logger: RazorLogger,
+    ) {}
+
+    public register() {
+        vscode.commands.registerCommand('razor/runCodeAction', (request: object) => this.runCodeAction(request), this);
+    }
+
+    private async runCodeAction(request: any): Promise<boolean | string | {}> {
+        const response: CodeActionResolutionResponse = await this.serverClient.sendRequest('razor/resolveCodeAction', {Action: request.Action, Data: request.Data});
+        this.logger.logVerbose(`received from razor/resolveCodeAction: ${JSON.stringify(response)}`);
+        let workspaceEdit: vscode.WorkspaceEdit;
+        try {
+            workspaceEdit = convertWorkspaceEditFromSerializable(response.edit);
+        } catch (e) {
+            this.logger.logAlways(`caught error running code action: ${e}`);
+            return Promise.resolve(false);
+        }
+        return vscode.workspace.applyEdit(workspaceEdit);
+    }
+}
